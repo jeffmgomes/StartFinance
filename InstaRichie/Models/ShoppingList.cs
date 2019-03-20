fe0de7bbc8237230eea818f65dccd10ff43c3758
Heather Leyssenaar <lysna87@gmail.com>
2019-03-20 12:04:17 +1030

    Added SQL table code

---
+++ InstaRichie/Models/ShoppingList.cs
@@ -0,0 +1,27 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    public class ShoppingList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ShoppingItemID { get; set; }
+
+        [NotNull]
+        public string NameOfItem { get; set; }
+
+        [NotNull]
+        public double PriceQuoted { get; set; }
+
+        [NotNull]
+        public string ShopName { get; set; }
+
+        [NotNull]
+        public DateTime ShoppingDate { get; set; }
+    }
+}

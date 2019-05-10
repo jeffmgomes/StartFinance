b83004ac38c3f5b69041933403e53567738c64d0
Heather Leyssenaar <lysna87@gmail.com>
2019-05-10 11:56:05 +0930

    Copied up to date files (AppointmentsPage.xaml.cs Personal Info etc) from the master branch to this one and updated Shell.xaml to include links.
    Signed-off-by: Heather Leyssenaar <lysna87@gmail.com>

---
+++ InstaRichie/Models/Appointments.cs
@@ -0,0 +1,27 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    public class Appointments
+    {
+        [PrimaryKey, AutoIncrement]
+        public int AppointmentID { get; set; }
+
+        [NotNull]
+        public string FirstName { get; set; }
+
+        [NotNull]
+        public string LastName { get; set; }
+
+        [NotNull]
+        public string DateOfAppointment { get; set; }
+
+        [NotNull]
+        public String TimeOfAppointmant { get; set; }
+    }
+}
